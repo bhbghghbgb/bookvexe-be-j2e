bd08cee03999737d37f3abf5b4a9b910934c9bea
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-09-26 12:45:15 +0800

    init spring boot project

---
+++ src/main/java/org/example/bookvexebej2e/BookvexeBeJ2eApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookvexebej2e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookvexeBeJ2eApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookvexeBeJ2eApplication.class, args);
+    }
+
+}
