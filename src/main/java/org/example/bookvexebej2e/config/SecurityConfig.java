b3b62a975d3e5b171cda473a814e592515d69c91
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-09-27 09:04:42 +0800

    setup keycloak for oauth2, backend realm name smb-j2e, roles: admin/user, account admin/admin, user/user

--- src/main/java/org/example/bookvexebej2e/config/SecurityConfig.java
+++ src/main/java/org/example/bookvexebej2e/config/SecurityConfig.java
@@ -2,7 +2,11 @@ package org.example.bookvexebej2e.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,10 +14,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.requestMatchers("/admin/**")
-            .authenticated() // Lock down admin routes
-            .anyRequest()
-            .permitAll() // Allow everything else
-        );
+                .hasRole("admin") // Lock down admin routes
+                .anyRequest()
+                .permitAll() // Allow everything else
+            )
+            .oauth2ResourceServer(
+                oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
         return http.build();
+    }
+
+    // Custom converter to map Keycloak roles
+    private Converter<Jwt, AbstractAuthenticationToken> jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtGrantedAuthoritiesConverter());
+        return converter;
     }
 }
--- src/main/java/org/example/bookvexebej2e/controller/AdminController.java
+++ src/main/java/org/example/bookvexebej2e/controller/AdminController.java
@@ -1,6 +1,7 @@
 package org.example.bookvexebej2e.controller;
 
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/admin")
+@SecurityRequirement(name = "keycloak")
 public class AdminController {
     @GetMapping("/hello")
     public Map<String, String> hello() {
