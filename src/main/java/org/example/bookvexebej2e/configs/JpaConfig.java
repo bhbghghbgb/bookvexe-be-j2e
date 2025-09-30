80111c7737820c1b56e3092df216f16bb6e8ccf4
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-09-30 15:01:00 +0700

    Add QueryDSL dependencies and configuration for JPA support

--- src/main/java/org/example/bookvexebej2e/configs/JpaConfig.java
+++ src/main/java/org/example/bookvexebej2e/configs/JpaConfig.java
@@ -2,8 +2,11 @@ package org.example.bookvexebej2e.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.support.QuerydslJpaPredicateExecutor;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(repositoryBaseClass = QuerydslJpaPredicateExecutor.class)
 public class JpaConfig {
 }
