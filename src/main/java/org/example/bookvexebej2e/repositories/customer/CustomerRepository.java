7b9238cdfaaeb38b808330a21f0bffbf3c39068b
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-10-25 15:24:52 +0800

    auth customer register

--- src/main/java/org/example/bookvexebej2e/repositories/customer/CustomerRepository.java
+++ src/main/java/org/example/bookvexebej2e/repositories/customer/CustomerRepository.java
@@ -1,9 +1,16 @@
 package org.example.bookvexebej2e.repositories.customer;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.example.bookvexebej2e.models.db.CustomerDbModel;
 import org.example.bookvexebej2e.repositories.base.BaseRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends BaseRepository<CustomerDbModel> {
+    Optional<Object> findByEmailAndIsDeletedFalse(@NotBlank @Email String email);
+
+    Optional<Object> findByPhoneAndIsDeletedFalse(@NotBlank String phone);
 }
