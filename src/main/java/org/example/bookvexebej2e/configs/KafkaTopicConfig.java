f321b96e53d24537f20d45d98b1db94875a53495
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-11-06 23:34:18 +0800

    [wip] (2) try link main backend -> notification & mail

--- src/main/java/org/example/bookvexebej2e/configs/KafkaTopicConfig.java
+++ src/main/java/org/example/bookvexebej2e/configs/KafkaTopicConfig.java
@@ -1,7 +1,9 @@
 package org.example.bookvexebej2e.configs;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
--- src/main/java/org/example/bookvexebej2e/repositories/trip/TripRepository.java
+++ src/main/java/org/example/bookvexebej2e/repositories/trip/TripRepository.java
@@ -4,6 +4,10 @@ import org.example.bookvexebej2e.models.db.TripDbModel;
 import org.example.bookvexebej2e.repositories.base.BaseRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TripRepository extends BaseRepository<TripDbModel> {
+    List<TripDbModel> findByDepartureTimeBetween(LocalDateTime now, LocalDateTime reminderWindow);
 }
