fe8d0e0c150fd75f8c753465a0cbc180f1c88dde
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-11-08 10:39:49 +0800

    @EnableScheduling

--- src/main/java/org/example/bookvexebej2e/BookvexeBeJ2eApplication.java
+++ src/main/java/org/example/bookvexebej2e/BookvexeBeJ2eApplication.java
@@ -4,9 +4,11 @@ import org.example.bookvexebej2e.repositories.base.BaseRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaRepositories(basePackages = "org.example.bookvexebej2e.repositories", repositoryBaseClass =
     BaseRepositoryImpl.class)
+@EnableScheduling
 @SpringBootApplication
 public class BookvexeBeJ2eApplication {
 
